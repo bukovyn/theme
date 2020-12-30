11b704329c06b32e684d33781faf3d0da39c4214
bukovyn <bukovyn@gmail.com>
2020-12-30 15:03:12 -0500

    update paths

--- update_theme.sh
+++ update_theme.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
+HOME="/home/naz"
+AUR="$HOME/aur"
+CONFIG="$HOME/.config"
+THEMES="$HOME/.themes"
+OOMOX="$AUR/oomox/pkg/oomox/opt/oomox"
+WALLPAPERS="$HOME/images/wallpaper"
+
 {
 	killall files firefox polybar spotify
 	spicetify update
 	wal-discord
 
 	# generate GTK theme and icon textures using pywal colors
-	oomox-cli -d true -o pywal -t /.themes $HOME/aur/oomox/pkg/oomox/opt/oomox/scripted_colors/xresources/xresources
-	$HOME/aur/oomox/pkg/oomox/opt/oomox/plugins/icons_gnomecolors/gnome-colors-icon-theme/change_color.sh -o pywal $HOME/aur/oomox/pkg/oomox/opt/oomox/scripted_colors/xresources/xresources
+	oomox-cli -d true -o pywal -t $HOME/.themes $OOMOX/scripted_colors/xresources/xresources
+	$OOMOX/plugins/icons_gnomecolors/gnome-colors-icon-theme/change_color.sh -o pywal $OOMOX/scripted_colors/xresources/xresources
 
 	# update intelliJ theme based on pywal colors
-	$HOME/aur/intellijPywal/intellijPywalGen.sh $HOME/.config/JetBrains/DataGrip2020.3
+	$AUR/intellijPywal/intellijPywalGen.sh $CONFIG/JetBrains/DataGrip2020.3
 } > /dev/null 2>&1
 
 {
 	spotify &
-	$HOME/.config/polybar/polybar.sh &
+	$CONFIG/polybar/polybar.sh &
 } 2>&1
 
 sleep 1;
